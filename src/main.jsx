eb460e45b8fac35bb4fa51a860763a74e028009e
Naresh Challa <124749877+Naresh2219@users.noreply.github.com>
2026-04-28 14:24:07 +0530

    Update

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './auth/AuthContext'
-import ErrorBoundary from './ErrorBoundary'
+import ErrorBoundary from './Errorboundary.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
